7f9ea484bf4c081cb1d639a89cc4433f778e5c83
zch <CHONG589@outlook.com>
2024-12-08 19:52:54 +0800

    test every module

--- tests/test_scheduler.cc
+++ tests/test_scheduler.cc
@@ -1,38 +1,31 @@
-/**
- * @file test_scheduler.cc
- * @brief 协程调度器测试
- * @version 0.1
- * @date 2021-06-15
- */
-
-#include "sylar/sylar.h"
-
-static sylar::Logger::ptr g_logger = SYLAR_LOG_ROOT();
+#include "scheduler.h"
 
 /**
  * @brief 演示协程主动yield情况下应该如何操作
  */
 void test_fiber1() {
-    SYLAR_LOG_INFO(g_logger) << "test_fiber1 begin";
+
+    LOG_INFO("coroutine:%d test_fiber1 %d begin", Fiber::GetThis()->getId(), Fiber::GetThis()->getId());
   
     /**
      * 协程主动让出执行权，在yield之前，协程必须再次将自己添加到调度器任务队列中，
      * 否则yield之后没人管，协程会处理未执行完的逃逸状态，测试时可以将下面这行注释掉以观察效果
      */
-    sylar::Scheduler::GetThis()->schedule(sylar::Fiber::GetThis());
+    Scheduler::GetThis()->schedule(Fiber::GetThis());
 
-    SYLAR_LOG_INFO(g_logger) << "before test_fiber1 yield";
-    sylar::Fiber::GetThis()->yield();
-    SYLAR_LOG_INFO(g_logger) << "after test_fiber1 yield";
+    LOG_INFO("coroutine:%d before test_fiber1 yield", Fiber::GetThis()->getId());
+    Fiber::GetThis()->yield();
+    LOG_INFO("coroutine:%d after test_fiber1 yield", Fiber::GetThis()->getId());
 
-    SYLAR_LOG_INFO(g_logger) << "test_fiber1 end";
+    LOG_INFO("coroutine:%d test_fiber1 end", Fiber::GetThis()->getId());
 }
 
 /**
  * @brief 演示协程睡眠对主程序的影响
  */
 void test_fiber2() {
-    SYLAR_LOG_INFO(g_logger) << "test_fiber2 begin";
+
+    LOG_INFO("coroutine:%d test_fiber2 %d begin", Fiber::GetThis()->getId(), Fiber::GetThis()->getId());
 
     /**
      * 一个线程同一时间只能有一个协程在运行，线程调度协程的本质就是按顺序执行任务队列里的协程
@@ -41,19 +34,19 @@ void test_fiber2() {
      */
     sleep(3);
 
-    SYLAR_LOG_INFO(g_logger) << "test_fiber2 end";
+    LOG_INFO("coroutine:%d test_fiber2 end", Fiber::GetThis()->getId());
 }
 
 void test_fiber3() {
-    SYLAR_LOG_INFO(g_logger) << "test_fiber3 begin";
-    SYLAR_LOG_INFO(g_logger) << "test_fiber3 end";
+    LOG_INFO("coroutine:%d test_fiber3 %d begin", Fiber::GetThis()->getId(), Fiber::GetThis()->getId());
+    LOG_INFO("coroutine:%d test_fiber3 end", Fiber::GetThis()->getId());
 }
 
 void test_fiber5() {
     static int count = 0;
 
-    SYLAR_LOG_INFO(g_logger) << "test_fiber5 begin, i = " << count;
-    SYLAR_LOG_INFO(g_logger) << "test_fiber5 end i = " << count;
+    LOG_INFO("coroutine:%d test_fiber5 %d begin", Fiber::GetThis()->getId(), Fiber::GetThis()->getId());
+    LOG_INFO("coroutine:%d test_fiber5 end", Fiber::GetThis()->getId());
 
     count++;
 }
@@ -62,23 +55,27 @@ void test_fiber5() {
  * @brief 演示指定执行线程的情况
  */
 void test_fiber4() {
-    SYLAR_LOG_INFO(g_logger) << "test_fiber4 begin";
+    
+    LOG_INFO("coroutine:%d test_fiber4 %d begin", Fiber::GetThis()->getId(), Fiber::GetThis()->getId());
     
     for (int i = 0; i < 3; i++) {
-        sylar::Scheduler::GetThis()->schedule(test_fiber5, sylar::GetThreadId());
+        Scheduler::GetThis()->schedule(test_fiber5, Thread::GetThreadId());
     }
 
-    SYLAR_LOG_INFO(g_logger) << "test_fiber4 end";
+    LOG_INFO("coroutine:%d test_fiber4 end", Fiber::GetThis()->getId());
 }
 
 int main() {
-    SYLAR_LOG_INFO(g_logger) << "main begin";
+
+    Log::Instance()->init(1, "./log", ".log", 1024);
+
+    LOG_INFO("main begin");
 
     /** 
      * 只使用main函数线程进行协程调度，相当于先攒下一波协程，然后切换到调度器的run方法将这些协程
      * 消耗掉，然后再返回main函数往下执行
      */
-    sylar::Scheduler sc; 
+    Scheduler sc; 
 
     // 额外创建新的线程进行调度，那只要添加了调度任务，调度器马上就可以调度该任务
     // sylar::Scheduler sc(3, false);
@@ -88,7 +85,7 @@ int main() {
     sc.schedule(test_fiber2);
 
     // 添加调度任务，使用Fiber类作为调度对象
-    sylar::Fiber::ptr fiber(new sylar::Fiber(&test_fiber3));
+    Fiber::ptr fiber(new Fiber(&test_fiber3));
     sc.schedule(fiber);
 
     // 创建调度线程，开始任务调度，如果只使用main函数线程进行调度，那start相当于什么也没做
@@ -106,6 +103,6 @@ int main() {
      */
     sc.stop();
 
-    SYLAR_LOG_INFO(g_logger) << "main end";
+    LOG_INFO("main end");
     return 0;
 }
